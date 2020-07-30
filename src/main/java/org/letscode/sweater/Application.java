2883b954fdd6ae7ce2467ea4ecd94934327e5584
AndrewKh7 <rjynhf125>
2020-07-30 15:08:28 +0300

    Hello, ${name}!

---
+++ src/main/java/org/letscode/sweater/Application.java
@@ -0,0 +1,13 @@
+package org.letscode.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
